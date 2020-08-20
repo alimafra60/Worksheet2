34838e8133591fde4172b20ad5848d2ae4518130
alimafra60 <69101326+alimafra60@users.noreply.github.com>
2020-08-20 11:57:29 +0530

    Update bookjournalrecords.java

--- Lab Ex 4/package and subpackage/bookandjournals/bookjournalrecords.java
+++ Lab Ex 4/package and subpackage/bookandjournals/bookjournalrecords.java
@@ -8,31 +8,117 @@
  *
  * @author Hi
  */
+import java.util.Scanner;
 import bookpack.bookdemo;
 import journalpack.journaldemo;
-import java.util.Scanner;
-public class bookjournalrecords {
+public class bookdetails {
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
-      bookdemo obj=new bookdemo();
-      journaldemo obj1=new journaldemo();
-      System.out.println("*****Welcome Student*****");
-      System.out.println("Enter the no of book to view the details");
-      int n=sc.nextInt();
-      obj.display(n);
-      System.out.println("Enter the no of journal to view the details");
-      int j=sc.nextInt();
-      obj1.display(j);
-        System.out.println("*****Welcome Staff*****");
-      System.out.println("Enter the no of book to view the details");
-      int b=sc.nextInt();
-      obj.display(b);
-      System.out.println("Enter the no of journal to view the details");
-      int j1=sc.nextInt();
-      obj1.display(j1);
-      
-              
+
+        Scanner obj=new Scanner(System.in);
+        System.out.println("*****Books and JOurnals Requested by the Student*****");
+        System.out.println("Enter Book ID");
+        int i=obj.nextInt();
+        System.out.println("Enter Journal ID");
+        int j=obj.nextInt();
+        System.out.println("*****Books and JOurnals Requested by the Staff*****");
+        System.out.println("Enter Book ID");
+        int p=obj.nextInt();
+        System.out.println("Enter Journal ID");
+        int q=obj.nextInt();
+        student ob1=new student(i,j);
+        staff ob2=new staff(p,q);
+        System.out.println("*****Details of Books and JOurnals Requested by the Student*****");
+        ob1.getstudentinfo();
+        System.out.println("*****Details of 1Books and JOurnals Requested by the Student*****");
+        ob2.getstaffinfo();
     }
     
 }
+class student
+{
+    int bkid,jlid;
+    bookdemo obj=new bookdemo();
+    journaldemo obj1=new journaldemo();
+    student(int b,int j)
+    {
+        bkid=b;
+        jlid=j;
+    }
+    void getstudentinfo()
+    {
+        int flag=0;
+        int flag1=0;
+        for(int i=0;i<5;i++)
+        {
+            if(obj.bookid[i]==bkid)
+            {
+                flag=1;
+                System.out.println("BOOK DETAILS :");
+                obj.display(i);
+            } 
+        }
+        if(flag==0)
+        {
+            System.out.println("Invalid book id");
+        }
+        for(int i=0;i<5;i++)
+        {
+            
+            if(obj1.journalid[i]==jlid)
+            {
+                flag1=1;
+                System.out.println("JOURNAL DETAILS :");
+                obj1.display(i);
+            } 
+        }
+        if(flag1==0)
+        {
+            System.out.println("Invalid journal id");
+        }
+    }
+}
+class staff
+{
+    int bkid,jlid;
+    bookdemo obj2=new bookdemo();
+    journaldemo obj3=new journaldemo();
+    staff(int b,int j)
+    {
+        bkid=b;
+        jlid=j;
+    }
+    void getstaffinfo()
+    {
+        int flag=0;
+        int flag1=0;
+        for(int i=0;i<5;i++)
+        {
+            if(obj2.bookid[i]==bkid)
+            {
+                flag=1;
+                System.out.println("BOOK DETAILS :");
+                obj2.display(i);
+            } 
+        }
+        if(flag==0)
+        {
+            System.out.println("Invalid book id");
+        }
+        for(int i=0;i<5;i++)
+        {
+            
+            if(obj3.journalid[i]==jlid)
+            {
+                flag1=1;
+                System.out.println("JOURNAL DETAILS :");
+                obj3.display(i);
+            } 
+        }
+        if(flag1==0)
+        {
+            System.out.println("Invalid journal id");
+        }
+    }
+}    
+    
