0a99ea1dfde47d97870cdbdc7d69d39e82c1cfac
alimafra60 <69101326+alimafra60@users.noreply.github.com>
2020-10-18 23:00:11 +0530

    Update word_count.java

--- lab exercise 11/word_count.java
+++ lab exercise 11/word_count.java
@@ -11,7 +11,7 @@ import java.io.FileReader;
 import java.io.IOException;
 /**
  *
- * @author POOJHA KEERTHI
+ * @author ALIM
  */
 public class word_count {
     public static void main(String args[])
